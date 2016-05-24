c34e929aa2ee46525a9b69dfc98fdcadbfaf8b0a
jodad015 <jodad015@gmail.com>
2016-05-24 09:23:42 -0500

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {DashboardComponent} from './dashboard.component';
+import {HeroDetailComponent} from './hero-detail.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [HeroService, ROUTER_PROVIDERS]
+})
+
+@RouteConfig([{
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    },{
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },{
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    }
+])
+
+export class AppComponent {
+    title = 'Tour of NG Heroes';
+}
